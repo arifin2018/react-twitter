ebbdb8d632835ef7ed70ad5d708ba1c40a76188f
arifinlenna <arifin@lenna.ai>
2023-06-19 21:50:19 +0700

    tweet home

--- components/circle.jsx
+++ components/circle.jsx
@@ -6,7 +6,7 @@ export default function Circle() {
 		<>
 			<section className='bottom-[9%] fixed w-3/6'>
 				<Link href="/">
-					<div className='float-right bg-sky-500 w-[4.5rem] h-[4.5rem] rounded-full flex justify-center items-center'>
+					<div className='right-4 bg-sky-500 w-[4.5rem] absolute h-[4.5rem] rounded-full flex justify-center items-center bottom-0'>
 						<span className='flex justify-center'>
 							<i className='bx bx-edit-alt text-2xl'></i>
 						</span>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 export default function Navbar() {
 	return (
 		<>
-			<nav className='bottom-0 fixed text-center w-3/6 border-t-2 border-slate-700'>
+			<nav className='bottom-0 fixed text-center w-3/6 border-t-black-1px'>
 				<div className="flex flex-row bg-white min-h-[12%]">
 					<div className='basis-1/4 py-1'>
 						<Link href="/">
-							<i className='bx bx-home text-3xl'></i>
+							<i className='bx bx-home text-3xl active'></i>
 						</Link>
 					</div>
 					<div className='basis-1/4 py-1'>
 						<Link href="/">
-							<i className='bx bx-search-alt-2 text-3xl ' ></i>
+							<i className='bx bx-search-alt-2 text-3xl text-gray-400' ></i>
 						</Link>
 					</div>
 					<div className='basis-1/4 py-1'>
 						<Link href="/">
-							<i className='bx bx-bell text-3xl' ></i>
+							<i className='bx bx-bell text-3xl text-gray-400'></i>
 						</Link>
 					</div>
 					<div className='basis-1/4 py-1'>
 						<Link href="/">
-							<i className='bx bx-envelope text-3xl' ></i>
+							<i className='bx bx-envelope text-3xl text-gray-400' ></i>
 						</Link>
 					</div>
 				</div>
